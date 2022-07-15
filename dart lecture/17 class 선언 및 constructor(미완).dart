be9ca7adae0ec67f139db33f2b2b150aaa3f0958
minsing-jin <60510718+minsing-jin@users.noreply.github.com>
2022-07-15 22:44:39 +0900

    Update 17 class 선언 및 constructor(미완).dart

--- dart lecture/17 class 선언 및 constructor(미완).dart
+++ dart lecture/17 class 선언 및 constructor(미완).dart
@@ -88,3 +88,64 @@ class Idol{
     print("제 이름은  ${this.name}입니다."); // -> this는 class와 색깔이 같은것이므로 class를 가리킴  // 
   }
 }
+
+
+-----------------------------------------------------------------------------------
+  void main() {
+  
+  // instantiation 인스턴스화 -> 클래스를 변수로 변형하는 프로세스
+  // instance 인스턴스 -> 클래스의 변수
+  Idol seulgi = new Idol(
+    group : '슬기',     // name에 슬기 저장, group에 레드벨벳으로 저장
+    name : '레드벨벳'
+  );
+  
+  seulgi.sayName();
+  print(seulgi.name);
+  print(seulgi.group);
+  
+  Idol rm = new Idol.fromMap({
+    'name' : 'RM',
+    'group' : 'BTS'
+  });
+  
+  rm.sayName();
+  
+  print(rm.name);
+  print(rm.group);
+}
+
+
+
+
+class Idol{
+  String name;  // 클래스에는 변수를 선언할 수 있다.
+  String group;  
+  
+  
+  // named parameter사용
+  Idol({
+    required String name, 
+    required String group,
+    })   : this.name = name,
+           this.group = group;
+  
+  
+  // fromMap을 이용한 named constructor사용
+  Idol.fromMap(
+    Map input,
+  ) : this.name = input['name'],
+      this.group = input['group'];
+  
+  
+  void sayName(){
+    print("제 이름은  ${this.name}입니다."); // -> this는 class와 색깔이 같은것이므로 class를 가리킴  // 
+  }
+}
+
+
+// Idol이라는 클래스를 이용해서 생성할 수 있는 인스턴스는 2가지
+// 1. class에 names parameter를 쓰는법
+// 2. from map을 사용해서 named constructor을 쓰는법
+// named constructor을 사용하면 한 클래스에 여러개의 constructor를 생성가능 -> 클래스의 인스턴스를 여러가지로 만들수 있는 방법
+// constructor의 변수들을 바꿀수 없게 만들려면 final을 사용
